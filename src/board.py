7e82b4f4b324395e6458e022d1e2f9cbe5b918be
LancelotAugustus <129496505+LancelotAugustus@users.noreply.github.com>
2026-01-08 01:13:34 +0800

    (.indev) v0.0.6

--- src/board.py
+++ src/board.py
@@ -1,7 +1,3 @@
-"""
-src/board.py
-"""
-
 from random import randint
 from typing import Tuple
 
@@ -11,12 +7,13 @@ class Board:
 
     def __init__(self):
         """初始化棋盘"""
-        self.size = 0
-        self.grid = []
+        self.digit_count = 0
+        self.digit_list = []
+        self.digit_grid = []
 
     def __str__(self):
         """可视化棋盘"""
-        return "\n".join(" ".join(str(digit) if digit != 0 else "." for digit in row) for row in self.grid)
+        return "\n".join(" ".join(str(digit) if digit != 0 else "." for digit in row) for row in self.digit_grid)
 
     @staticmethod
     def calc_coord(global_index: int) -> Tuple[int, int]:
@@ -34,15 +31,20 @@ class Board:
         col_index = global_index % 9
         return row_index, col_index
 
-    def set_grid(self, digit_list: list[int]) -> None:
+    def sync_data(self) -> None:
+        """更新成员变量"""
+        self.digit_count = len(self.digit_list)
+        self.digit_grid = [self.digit_list[i: i + 9] for i in range(0, self.digit_count, 9)]
+
+    def set_digits(self, digit_list: list[int]) -> None:
         """
         设置局面
 
         Args:
             digit_list: 表示局面的整数列表，使用0表示空格
         """
-        self.size = len(digit_list)
-        self.grid = [digit_list[i: i + 9] for i in range(0, self.size, 9)]
+        self.digit_list = digit_list
+        self.sync_data()
 
     def generate_grid(self, size: int) -> None:
         """
@@ -51,8 +53,8 @@ class Board:
         Args:
             size: 棋盘尺寸
         """
-        digit_list = [randint(0, 9) for _ in range(size)]
-        self.set_grid(digit_list)
+        self.digit_list = [randint(1, 9) for _ in range(size)]
+        self.sync_data()
 
     def get_digit_by_coord(self, row_index: int, col_index: int) -> int:
         """
@@ -65,12 +67,11 @@ class Board:
         Returns:
             digit: 指定位置的数字
         """
-        digit = self.grid[row_index][col_index]
-        return digit
+        return self.digit_grid[row_index][col_index]
 
     def get_digit_by_index(self, global_index: int) -> int:
         """
-        按坐标获取数字
+        按索引获取数字
 
         Args:
             global_index: 全局索引（0-based）
@@ -78,9 +79,7 @@ class Board:
         Returns:
             digit: 指定位置的数字
         """
-        row_index, col_index = self.calc_coord(global_index)
-        digit = self.get_digit_by_coord(row_index, col_index)
-        return digit
+        return self.digit_list[global_index]
 
     def set_digit_by_coord(self, row_index: int, col_index: int, digit: int) -> None:
         """
@@ -91,7 +90,9 @@ class Board:
             col_index: 列索引（0-based）
             digit: 要放置的数字
         """
-        self.grid[row_index][col_index] = digit
+        global_index = 9 * row_index + col_index
+        self.digit_list[global_index] = digit
+        self.digit_grid[row_index][col_index] = digit
 
     def set_digit_by_index(self, global_index: int, digit: int) -> None:
         """
@@ -102,7 +103,8 @@ class Board:
             digit: 要放置的数字
         """
         row_index, col_index = self.calc_coord(global_index)
-        self.set_digit_by_coord(row_index, col_index, digit)
+        self.digit_list[global_index] = digit
+        self.digit_grid[row_index][col_index] = digit
 
     def is_matching(self, global_index1: int, global_index2: int) -> bool:
         """
@@ -115,15 +117,17 @@ class Board:
         Returns:
             is_matching: 如果能够配对消除则返回True，否则返回False
         """
-        row_index1, col_index1 = self.calc_coord(global_index1)
-        row_index2, col_index2 = self.calc_coord(global_index2)
-        digit1 = self.get_digit_by_coord(row_index1, col_index1)
-        digit2 = self.get_digit_by_coord(row_index2, col_index2)
-
         if global_index1 == global_index2:
             return False
+
+        digit1 = self.get_digit_by_index(global_index1)
+        digit2 = self.get_digit_by_index(global_index2)
+
         if digit1 == 0 or digit2 == 0:
             return False
+
+        row_index1, col_index1 = self.calc_coord(global_index1)
+        row_index2, col_index2 = self.calc_coord(global_index2)
 
         if digit1 == digit2 or digit1 + digit2 == 10:
             # 相同行
@@ -170,30 +174,21 @@ class Board:
             new_board: Board实例
         """
         new_board = Board()
-        new_board.grid = [row[:] for row in self.grid]
-        new_board.size = self.size
+        new_board.digit_count = self.digit_count
+        new_board.digit_list = self.digit_list[:]
+        new_board.digit_grid = [row[:] for row in self.digit_grid]
         return new_board
 
     def clear(self) -> None:
         """棋盘操作（被动） —— 清理空行"""
-        new_board = []
-        for row in self.grid:
-            if any(row):
-                new_board.append(row)
-            else:
-                self.size -= len(row)
-        self.grid = new_board
+        self.digit_list = [digit for row in self.digit_grid if any(row) for digit in row]
+        self.sync_data()
 
     def fill(self) -> None:
         """棋盘操作（主动） —— 拷贝填充"""
-        remaining_digits = [digit for i in range(self.size) if (digit := self.get_digit_by_index(i))]
-        self.size += len(remaining_digits)
-
-        split_index = 9 - len(self.grid[-1])
-        self.grid[-1] += remaining_digits[: split_index]
-
-        digit_list = remaining_digits[split_index:]
-        self.grid += [digit_list[i: i + 9] for i in range(0, len(digit_list), 9)]
+        remaining_digits = [digit for digit in self.digit_list if digit]
+        self.digit_list += remaining_digits
+        self.sync_data()
 
     def match(self, global_index1: int, global_index2: int) -> None:
         """棋盘操作（主动） —— 配对消除"""
